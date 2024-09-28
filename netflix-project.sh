340663f7ad69f109f0fa86580b3cab593de6ec8b
seifsherif1544 <163683742+seifsherif1544@users.noreply.github.com>
2024-09-28 17:16:49 +0300

    Update netflix-project.sh

--- netflix-project.sh
+++ netflix-project.sh
@@ -7,8 +7,6 @@ docker images
 
 docker ps
 
-
-
 echo "------------------------- Deleting images & Containers -------------------------------"
 
 docker stop $(docker ps -a -q) 
@@ -26,12 +24,17 @@ docker ps
 
 echo "--------------------------- Bulding image from Dockerfile -----------------------------"
 
-sh docker build --build-arg TMDB_V3_API_KEY=your_api_key_here -t ssherif/netflix .
-
+sh docker build --build-arg TMDB_V3_API_KEY=023344d139572e6c6c41a11b0c7afc57 -t ssherif/netflix .
 
 echo "----------------------------- Create Container from image -----------------------------"
 
-docker run --name netflix-website --rm -d -p 2000:2000 ssherif/netflix
+docker run --name netflix-website --rm -d -p 2000:2000 ssherif/netflix:latest
+
+echo "------------------------ Push a new tag to this repository-----------------------------"
+
+docker login 
+
+docker push ssherif/netflix:latest
 
 echo "---------------------------------- Showing Web-Site -----------------------------------"
 
